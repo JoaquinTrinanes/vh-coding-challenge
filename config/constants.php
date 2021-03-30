f594f04b931e8803f9dac3eeb160823ac1663b47
Joaquín Triñanes <quin.trinanes@gmail.com>
2021-03-30 19:58:19 +0200

    Add some random question placeholders

---
+++ config/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'question_placeholders' => [
+        'Where does B12 grow?',
+        'Is chicken vegan?',
+        'Someone was mean to me online. Should I quit veganism?'
+    ]
+];
