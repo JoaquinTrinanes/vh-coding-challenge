dc1a7aa4720b0438e6338c139a2c9649f6449057
Joaquín Triñanes <quin.trinanes@gmail.com>
2021-03-30 20:04:48 +0200

    Add more sample questions

--- config/constants.php
+++ config/constants.php
@@ -4,6 +4,10 @@ return [
     'question_placeholders' => [
         'Where does B12 grow?',
         'Is chicken vegan?',
-        'Someone was mean to me online. Should I quit veganism?'
+        'Someone was mean to me online. Should I quit veganism?',
+        'What is a question?',
+        'Are long questions that would normally break the page layout correctly formatted using ellipsis?',
+        'What are some good sources of D3 vitamin?',
+        'What\'s your favorite tofu marinade?'
     ]
 ];
